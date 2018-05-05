bdd7a1322383b74e35c115cb8f0e5c63965c0b63
Matt Conway <mattconway55@gmail.com>
2018-05-05 09:19:40 -0400

    Added array test from mocha docs

--- test/test.js
+++ test/test.js
@@ -13,4 +13,11 @@ describe('app', function() {
       done();
     })
   })
+  describe('Array', () => {
+    describe('#indexOf()', () => {
+      it('should return -1 when the value is not present', () => {
+        assert.equal([1,2,3].indexOf(4), -1)
+      })
+    })
+  })
 });
